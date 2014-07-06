53248639d6494d68952298c25bfe4c903ee8115b
Todd Macdonald <todd9527@gmail.com>
2014-07-06 16:08:03 -0700

    msdf

--- web.js
+++ web.js
@@ -1,11 +1,9 @@
 var express = require("express");
-//var log = require("logfmt");
 
 var port = 5000;
 
 var app = express();
 
-//app.use(log);
 app.use(express.static(__dirname+"/public"));
 
 app.listen(port);
